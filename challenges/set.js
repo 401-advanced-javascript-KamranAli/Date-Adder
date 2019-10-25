5017ec4cc9fc3bbf18aa709292c1f31620062398
Kamran Ali <kgali21@gmail.com>
2019-10-24 18:20:50 -0700

    started and first 3 questions solved

---
+++ challenges/set.js
@@ -0,0 +1,36 @@
+
+class Set {
+  constructor(array) {
+    this.array = array;
+  }
+
+  add(value) {
+    if(!this.array.includes(value)) {
+      this.array.push(value);
+    }
+  }
+
+  remove(value) {
+    if(this.array.includes(value)){
+      const number = this.array.indexOf(value);
+      this.array.splice(number, 1);
+    }
+  }
+
+  has(value) {
+    return this.array.includes(value);
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
+
+module.exports = Set;
